e10520ca6e571b406b3676f4e4837fb9de59d9f2
GodOfPerception <117932795+GodOfPerception@users.noreply.github.com>
2023-02-25 20:02:05 +0530

    Update joke.py

--- joke.py
+++ joke.py
@@ -11,28 +11,24 @@ If user types anything else, The program should print "Could not understand you,
 #code"""
 import json
 from urllib.request import urlopen
-with urlopen("https://official-joke-api.appspot.com/random_joke") as u:
-    data = u.read()
+
 def jokes():
+    with urlopen("https://official-joke-api.appspot.com/random_joke") as u:
+        data = u.read()
     m = json.loads(data)
-    v=json.dumps(m, indent=2)
-    return m["setup"],m["punchline"]
-
-j=jokes()
-
+    return m["setup"], m["punchline"]
 
 def jokemech():
-    print("Wanna hear a joke type Y/N")
+    print("Wanna hear a joke? Type Y/N")
 
-    r = input()
-    if(r=="y" or r=="Y"):
-        print(j)
+    r = input().lower()
+    if r == "y":
+        print(jokes())
         jokemech()
-
-    elif(r=="n" or r=="N"):
-        print("Thanks for your time")
+    elif r == "n":
+        print("Thanks for your time!")
     else:
-        print("Could not understand you, Please type Y, N")
+        print("Could not understand you. Please type Y or N.")
         jokemech()
-print(j)
+
 jokemech()
