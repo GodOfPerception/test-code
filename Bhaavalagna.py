5d1cb5ad319fd49cfa8d1227b1c17e298a445436
GodOfPerception <117932795+GodOfPerception@users.noreply.github.com>
2023-02-28 20:08:06 +0530

    Update Bhaavalagna.py

--- Bhaavalagna.py
+++ Bhaavalagna.py
@@ -15,27 +15,13 @@ import json
 
 data = {
     'day': 'Friday',
-    'sunrise': '5:3:15',
-    'sunlongitude': '20°17',
+    'sunrise': '6:37:00',
+    'sunlongitude': '24°17',
     'sunset': '18:30:16',
     'moonrise': '15:49:27',
     'moonset': '2:55:31',
     'vedic_sunrise': '5:7:21',
     'vedic_sunset': '18:26:9'
-}
-
-degree_ranges = {
-    range(0, 30): 'Aries',
-    range(30, 60): 'Gemini',
-    range(60, 120): 'Leo',
-    range(120, 150): 'Virgo',
-    range(150, 180): 'Libra',
-    range(180, 210): 'Scorpio',
-    range(210, 240): 'Sagittarius',
-    range(240, 270): 'Capricorn',
-    range(270, 300): 'Aquarius',
-    range(300, 330): 'Pisces',
-    range(330, 360): 'Aries'
 }
 
 jdatai = json.dumps(data)
@@ -48,28 +34,47 @@ sunrise_m = int(sunrise_components[1])
 sunrise_s = int(sunrise_components[2])
 total_sunrise = sunrise_h * 3600 + sunrise_m * 60 + sunrise_s
 sun_longitude = jdataii["sunlongitude"]
-sun_longitude_degrees = int(sun_longitude.split('°')[0])
+sun_longitude_angle = int(sun_longitude.split('°')[0])
+sun_longitude_degree= int(sun_longitude.split('°')[1])
 
-time_str = input("Enter Time of birth in format of hour:min:sec AM/PM")
+time_str = input("Enter Time of birth in format of hour:min AM/PM: ")
 time_components = time_str.split(':')
 time_h = int(time_components[0])
 time_m = int(time_components[1].split()[0])
-if time_str.endswith('PM'):
-    time_h += 12
+time_ampm = time_components[1].split()[1]
+if time_ampm == "PM":
+    time_h = time_h + 12 if time_h < 12 else time_h
+if time_ampm == "AM" and time_h == 12:
+    time_h = 0
 time_s = 0
 total_seci = time_h * 3600 + time_m * 60 + time_s
 
 if total_seci > total_sunrise:
-    elapsed_second = total_seci - total_sunrise
+    elapsed_second = (total_seci - total_sunrise)
 else:
-    elapsed_second = total_sunrise-total_seci
+    elapsed_second = (total_sunrise - total_seci)
 
-elapsed_minutes = elapsed_second // 60
+elapsed_minutes = (elapsed_second // 60)/2
+sun_longitude_sunrise=270+sun_longitude_angle+elapsed_minutes
+s=sun_longitude_sunrise
 
-result_degrees = (sun_longitude_degrees + elapsed_minutes) % 360
-print(result_degrees)
+print("HL is",int(sun_longitude_sunrise%30),"°",sun_longitude_degree,"in")
+degree_ranges = {
+    0: 'Aries',
+    1: 'Taurus',
+    2: 'Gemini',
+    3: 'Cancer',
+    4: 'Leo',
+    5: 'Virgo',
+    6: 'Libra',
+    7: 'Scorpio',
+    8: 'Sagittarius',
+    9: 'Capricorns',
+    10: 'Aquarius',
+    11: 'Pieces'
+}
+sign = sun_longitude_sunrise//30
+if sign>11:
+    sign=sign//2
 
-for degree_range, zodiac_sign in degree_ranges.items():
-    if result_degrees in degree_range:
-        print(zodiac_sign)
-        break
+print(degree_ranges[sign])
